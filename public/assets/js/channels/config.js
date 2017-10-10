87918de4651b779abc942e021fddd8042d9487d0
Jonathan Gryn <jonathangryn@Jonathans-MacBook-Pro.local>
2017-10-10 15:04:01 -0700

    updated front-end

---
+++ public/assets/js/channels/config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDJdirOCK8p3_0N5H0RTZkDiSLCPOelWDU",
+    authDomain: "chatbox-bd0a9.firebaseapp.com",
+    databaseURL: "https://chatbox-bd0a9.firebaseio.com",
+    projectId: "chatbox-bd0a9",
+    storageBucket: "chatbox-bd0a9.appspot.com",
+    messagingSenderId: "35787626379"
+  };
+  firebase.initializeApp(config);
