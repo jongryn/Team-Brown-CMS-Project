42317b66c99f3c02a108acaafd0d8e718d8ee89a
Jonathan Gryn <jonathangryn@dhcp-v114-060.mobile.uci.edu>
2017-10-09 21:51:42 -0700

    changed public files

---
+++ public/assets/js/channels/full_calendar.js
@@ -0,0 +1,27 @@
+var initialize_calendar;
+initialize_calendar = function() {
+  $('.calendar').each(function(){
+    var calendar = $(this);
+    calendar.fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+      },
+      selectable: true,
+      selectHelper: true,
+      editable: true,
+      eventLimit: true
+
+      select: function(start, end) {
+        $.getScript('/events/new', function() {
+          $('#event_date_range').val(moment(start).format("MM/DD/YYYY HH:mm") + ' - ' + moment(end).format("")
+          date_range_picker();
+          $('.start_hidden').val(moment(start).format('YYYY-MM-DD HH:mm'));
+          $('.end_hidden').val(moment(end).format('YYYY-MM-DD HH:mm'));
+        });
+      }
+    });
+  })
+};
+$(document).on('turbolinks:load', initialize_calendar);
